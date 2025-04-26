fcac766d028242474aba4d2c9f3b7227c192574d
Abdullah Al Masud <masudctg00@gmail.com>
2025-04-27 01:18:56 +0800

    initial setup done. - tailwindcss setup - react router setup - pages router setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import Login from "./Pages/Login/Login.jsx";
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
+import Profile from "./Pages/Profile/Profile.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Create the router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Main layout
+    children: [
+      { index: true, element: <Home /> }, // default path "/"
+      { path: "login", element: <Login /> },
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "profile", element: <Profile /> },
+    ],
+  },
+]);
+
+// Render the app
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
